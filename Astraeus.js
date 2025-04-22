d18fef3cf4bcd4fcefd8d2d3d7a59af6c3afc2a0
Robin <robindev2019@outlook.com>
2025-04-23 00:56:59 +0200

    bugfixes :)

--- Astraeus.js
+++ Astraeus.js
@@ -41,19 +41,13 @@ export const Astraeus = function (myName, node, enemyNode) {
   const possibleMoves = [];
 
   for (const direction of possibleDirections) {
-    // `p` is short for possibleNode
+    // `p` is short for possibleNode.
     const p = neighbouringNodes[direction];
 
     p.potentialEnemyNode = enemyTerritory.some((n) => n.x == p.x && n.y == p.y);
     p.alreadyClaimed = claimedTerritory.some(
       (n) => n.x == p.x && n.y == p.y && n.z == p.z
     );
-
-    const debug = claimedTerritory.filter(
-      (n) => n.x == p.x && n.y == p.y && n.z == p.z
-    );
-
-    console.log(`alreadyClaimedlen: ${debug.length} and ${p.alreadyClaimed}`);
 
     p.direction = direction;
     possibleMoves.push(p);
@@ -62,7 +56,7 @@ export const Astraeus = function (myName, node, enemyNode) {
   // Determine which node we want to move to.
 
   let finalDecision;
-  let basedOn;
+  let moveReason;
 
   const freeMoves = possibleMoves.filter(
     (n) => !n.alreadyClaimed && !n.potentialEnemyNode
@@ -80,29 +74,35 @@ export const Astraeus = function (myName, node, enemyNode) {
     // Check which nodes are neither potential enemies or
     // already claimed. If any, pick a random one.
     finalDecision = random(freeMoves).direction;
-    basedOn = "free";
+    moveReason = "free";
   } else if (dangerousButFree.length != 0) {
     // Otherwise, prefer a potential enemy node over an
     // already claimed node.
     finalDecision = random(dangerousButFree).direction;
-    basedOn = "risky";
+    moveReason = "risky";
   } else {
     // If we boxed ourselves in, just do *something*.
     finalDecision = random(lastResortMoves).direction;
-    basedOn = "last-resort";
+    moveReason = "last-resort";
   }
 
   // Keep track of where we are relative to our starting
   // position.
-  if (finalDecision == "up") relativeZ -= 1;
-  if (finalDecision == "down") relativeZ += 1;
+
+  if (finalDecision == "forward") relativeZ -= 1;
+  if (finalDecision == "backward") relativeZ += 1;
+
+  let territory = new Set();
+
+  for (const c of claimedTerritory) {
+    // `c` is short for claimedNode.
+    territory.add(`${c.x},${c.y},${c.z}`);
+  }
+
+  // prompt("Next?");
 
   console.log(
-    `[${iterCounter}] Am at ${node.x},${node.y},${
-      node.z
-    }, moving ${finalDecision} to ${JSON.stringify(
-      neighbouringNodes[finalDecision]
-    )} because ${basedOn}`
+    `[${iterCounter}] coords: ${node.x},${node.y},${node.z} move: ${finalDecision} (${moveReason}) ter: ${territory.size}`
   );
 
   return finalDecision;
